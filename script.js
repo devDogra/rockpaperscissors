d9063ae407b6b16bfebe30cc279f34a4ed1572a7
DevAryanDogra <devdogra1@gmail.com>
2022-12-16 22:04:39 +0530

    Finish game

--- script.js
+++ script.js
@@ -1,32 +1,90 @@
 const numChoices = 3;
 const numRounds = 5;
+const choices = ["ROCK", "SCISSORS", "PAPER"]; // 0, 1, 2 : R S P
 
-const choices = ["ROCK", "SCISSORS", "PAPER"];
-// 0, 1, 2
-// R S P
-// R beats S, S beats P, P beats S
+let infoDiv = document.querySelector(".info");
+let iconRingContainer = document.querySelector(".icon-ring-container");
+let roundResult = document.querySelector(".round-result");
+let userScoreSpan = document.querySelector(".user-score");
+let computerScoreSpan = document.querySelector(".computer-score");
+let iconDivs = document.querySelectorAll(".icon");
+
+let userScore = 0;
+let computerScore = 0;
+let gameOver = false;
 
 // Will return 0, 1 or 2
 function getComputerChoice() {
   return Math.floor(Math.random() * numChoices);
 }
 
-// Eg : ROCK, rock, RoCK, R, r, Rcccc are all valid options for rock
-// Returns 0, 1, 2
-function getUserChoice() {
-  let userChoice = prompt(
-    "Pick Rock Paper or Scissors:  (Not picking will pick one randomly) "
-  );
+// Takes in a click event, outputs 0 1 or 2
+function getUserChoice(e) {
+  let icon = e.target.closest(".icon");
+  let userChoice = icon.dataset.choice;
+  return parseInt(userChoice);
+}
 
-  if (userChoice == "" || userChoice == null) return getComputerChoice();
-  userChoice = userChoice.toUpperCase();
+// Checks if someone has won
+function checkWin(userScore, computerScore) {
+  if (userScore >= 10 || computerScore >= 10) return true;
+  return false;
+}
 
-  for (let i = 0; i != choices.length; i++) {
-    if (userChoice == choices[i] || userChoice[0] == choices[i][0]) {
-      return i;
-    }
+function toggleGameOverState() {
+  iconRingContainer.classList.toggle("game-over");
+  infoDiv.classList.toggle("game-over");
+  gameOver = !gameOver;
+  userScore = 0;
+  computerScore = 0;
+}
+
+function handleWin() {
+  if (userScore == computerScore) {
+    roundResult.innerText = "Draw";
+  } else if (userScore > computerScore) {
+    roundResult.innerText = "You win";
+  } else {
+    roundResult.innerText = "You lose";
   }
-  //   return 2;
+
+  toggleGameOverState();
+}
+
+// Updates game state after a round, handles win if there is any
+function updateScores(roundResult) {
+  if (roundResult == 1) {
+    userScore++;
+  } else if (roundResult == -1) {
+    computerScore++;
+  } else {
+    userScore += 1;
+    computerScore += 1;
+  }
+
+  let win = checkWin(userScore, computerScore);
+  if (win) {
+    handleWin();
+  }
+}
+
+function renderScores() {
+  userScoreSpan.innerText = userScore;
+  computerScoreSpan.innerText = computerScore;
+}
+
+// Applies styling to the option picked by user
+function renderUserChoice(e) {
+  let iconDiv = e.target.closest(".icon");
+  iconDiv.classList.add("user");
+}
+
+// Applies styling to the option picked by computer
+function renderComputerChoice(computerChoice) {
+  let iconDiv = document.querySelector(
+    `.icon[data-choice="${computerChoice}"]`
+  );
+  iconDiv.classList.add("computer");
 }
 
 // outputs +1 if player wins, 0 if draw, and -1 if computer wins
@@ -40,48 +98,35 @@ function playRound(computerChoice, playerChoice) {
   }
 }
 
-function playGame(numRounds) {
-  let userScore = 0;
-  let computerScore = 0;
-  for (let round = 1; round <= numRounds; round++) {
-    let userChoice = getUserChoice();
-    let computerChoice = getComputerChoice();
-
-    let roundResult = playRound(computerChoice, userChoice);
-
-    if (roundResult == 1) {
-      let msg = `${choices[userChoice]} beats ${choices[computerChoice]}!
-       User wins round ${round}`;
-      console.log(msg);
-      userScore++;
-    } else if (roundResult == 0) {
-      let msg = `Round ${round} is a draw!`;
-      console.log(msg);
-      userScore += 0.5;
-      computerScore += 0.5;
-    } else {
-      let msg = `${choices[computerChoice]} beats ${choices[userChoice]}!
-        Computer wins round ${round}`;
-      console.log(msg);
-      computerScore++;
-    }
-  }
-
-  // Display score
-  let finalScoreMsg =
-    `FINAL SCORE \n` +
-    `User: ${userScore} \n` +
-    `Computer: ${computerScore} \n`;
-
-  console.log(finalScoreMsg);
-
-  if (userScore == computerScore) {
-    console.log("The game is a draw! ");
-  } else if (userScore < computerScore) {
-    console.log("Computer wins!");
-  } else {
-    console.log("User wins");
-  }
+function resetIconClasses() {
+  iconDivs.forEach((iconDiv) => {
+    iconDiv.classList.remove("user");
+    iconDiv.classList.remove("computer");
+  });
 }
 
-playGame(numRounds);
+// Handles user clicking on an icon event
+document.addEventListener("click", (e) => {
+  if (!e.target.matches(".icon>img")) return;
+
+  resetIconClasses();
+
+  let userChoice = getUserChoice(e);
+  renderUserChoice(e);
+
+  let computerChoice = getComputerChoice();
+  renderComputerChoice(computerChoice);
+
+  let roundResult = playRound(computerChoice, userChoice);
+  updateScores(roundResult);
+
+  renderScores();
+});
+
+// Play again button click handler
+document.addEventListener("click", (e) => {
+  if (!e.target.matches(".play-again")) return;
+
+  // restart game
+  toggleGameOverState();
+});
